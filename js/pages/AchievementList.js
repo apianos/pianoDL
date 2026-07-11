e27b80b7f507d59cbb9e752acd8f06b67ccb138b
advikmrai <advikmrai@gmail.com>
2026-07-11 06:22:09 +0200

    newest

--- js/pages/AchievementList.js
+++ js/pages/AchievementList.js
@@ -3,7 +3,7 @@ import { embed } from '../util.js';
 import { fetchEditors } from '../content.js';
 import Spinner from '../components/Spinner.js';
 
-const csvPath = '/data/pianoDL - piano achievement list (8).csv';
+const csvPath = '/data/pianoDL - piano achievement list (10).csv';
 
 function parseCsv(text, delimiter = ',') {
     const rows = [];
