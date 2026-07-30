65bf9ae6b46fb1e104514fbee9dccdb3d9959d37
advikmrai <advikmrai@gmail.com>
2026-07-30 12:30:14 -0400

    wtv

--- js/pages/VerifiedCsvList.js
+++ js/pages/VerifiedCsvList.js
@@ -3,7 +3,7 @@ import { embed } from '../util.js';
 import { fetchEditors } from '../content.js';
 import Spinner from '../components/Spinner.js';
 
-const csvPath = '/data/pianoDL - piano achievement list (22).csv';
+const csvPath = '/data/pianoDL - piano achievement list (23).csv';
 
 function parseCsv(text, delimiter = ',') {
     const rows = [];
